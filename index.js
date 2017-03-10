24cce2e2e8e9d3a32e7719b664698df33125116f
Daniel Wang <ddwang@gmail.com>
2017-03-10 09:05:07 -0800

    updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const chalk = require('chalk');
 const figlet = require('figlet');
 const getmac = require('getmac');
+const KalmanFilter = require('./kalman');
 const LRU = require('lru-cache');
 const mqtt = require('mqtt');
 const noble = require('noble');
@@ -14,16 +15,33 @@ const yargs = require('yargs');
 
 // ----- global variables
 let _addr = null;
+let _addrK = 'aaaa:bb:cccccc';
 let _mqClient = null;
 let _mqCache = LRU(256);
 let _advertCache = LRU(1024);
+let _kalman = new KalmanFilter(0.01, 1);
 
-let getMacPromise = function(prefix) {
-	if (!prefix) {
-		prefix = 'SETUP';
-	}
+noble
+    .on('stateChange', function(state) {
+        console.log(chalk.dim('[NOBLE]'), 'state is', state);
+        if (state !== 'poweredOn') {
+            throw new Error('bluetooth powered off or not available')
+            return;
+        }
+
+        setup();
+    })
+    .on('discover', function(peripheral) {
+        let addr = normalizeAddr(peripheral.uuid);
+        cacheAdd(addr, peripheral.rssi, peripheral.advertisement.localName);
+    });
+
+let promiseGetMac = function(prefix) {
+    if (!prefix) {
+        prefix = 'SETUP';
+    }
     return new Promise(function(resolve, reject) {
-        console.log(chalk.dim('[' + prefix + ']'), 'getting own MAC address ...');
+        console.log(chalk.dim('[' + prefix + ']'), 'getting relay MAC address ...');
 
         getmac.getMac(function(err, addr) {
             if (err) {
@@ -37,35 +55,6 @@ let getMacPromise = function(prefix) {
     });
 }
 
-yargs
-    .strict()
-    .option('host', {
-        alias: 'h',
-        describe: '// ZING ingest server hostname',
-        type: 'string'
-    })
-	.option('token', {
-        alias: 't',
-        describe: '// ZING ingest token',
-        type: 'string'
-    })
-    .wrap(null)
-    .command('info', '// information about this node', {}, function(argv) {
-        getMacPromise('INFO').then((addr) => {
-			console.log(chalk.dim('[INFO]'), '\t', chalk.bold(addr));
-            process.exit(0);
-        });
-    })
-	.command('start', '// start this relay node', {}, function(argv) {
-        getMacPromise().then((addr) => {
-            console.log('Relay Key:', addr);
-            process.exit(0);
-        });
-    })
-    .usage(figlet.textSync('ZING') + ' (' + package_json.version + ')\n\n' + ' Usage: ' + chalk.bold('zing-spot') + ' <command>')
-    .demand(1, null)
-    .argv;
-
 function normalizeAddr(addr) {
     let a = addr.replace(/[^0-9A-Fa-f]/g, '');
     if (a.length != 12) {
@@ -75,6 +64,83 @@ function normalizeAddr(addr) {
     a = a.toLowerCase();
     return a.substring(0, 4) + ':' + a.substring(4, 6) + ':' + a.substring(6, 12);
 }
+
+function setup() {
+    yargs
+        .strict()
+        .option('host', {
+            alias: 'h',
+            describe: '// ZING ingest server hostname',
+            type: 'string'
+        })
+        .option('token', {
+            alias: 't',
+            describe: '// ZING ingest token',
+            type: 'string'
+        })
+        .command('info', '// information about this node', {}, function(argv) {
+            promiseGetMac('INFO ') //
+                .then((addr) => {
+                    console.log(chalk.dim('[INFO ]'), '->', chalk.bold(addr));
+                    process.exit(0);
+                });
+        })
+        .command('start', '// start this relay node', {}, function(argv) {
+            promiseGetMac('RUN  ') //
+                .then((addr) => {
+					_addr = addr;
+                    console.log(chalk.dim('[RUN  ]'), '->', chalk.bold(addr));
+                })
+                .then(() => new Promise(function(resolve, reject) {
+                    console.log(chalk.dim('[RUN  ]'), 'connecting to MQTT server ...');
+
+                    let client = mqtt.connect('mqtt://test1:blahblah12@intelligent-rhino.rmq.cloudamqp.com');
+                    client.on('connect', function() {
+                        resolve(client);
+                    });
+                    client.on('error', function(err) {
+                        reject(err);
+                    });
+
+                    client.on('reconnect', function() {
+                        console.log(chalk.dim('[MQTT ]'), 'mqtt.Client.reconnect()', client);
+                    });
+
+                    client.on('close', function() {
+                        console.log(chalk.dim('[MQTT ]'), 'mqtt.Client.close()', client);
+                        process.exit();
+                    });
+                }))
+                .then(client => {
+					_mqClient = client;
+                    console.log(chalk.dim('[RUN  ]'), '->', chalk.bold(client.connected ?
+                        'CONNECTED' :
+                        'DISCONNECTED'));
+                })
+                .then(() => {
+                    console.log(chalk.dim('[RUN  ]'), 'start scanning ...');
+                    noble.startScanning([], true);
+                })
+                .catch(err => {
+                    console.error(chalk.bold('[ERROR]'));
+                    console.error(err);
+                    process.exit(1);
+                });
+        })
+        .wrap(null)
+        .usage(figlet.textSync('ZING') + ' (' + package_json.version + ')\n\n' + ' Usage: ' + chalk.bold('zing-relay') + ' <command>')
+        .demand(1, null)
+        .argv;
+
+    setInterval(function() {
+        if (!_mqCache.length || !_mqClient || !_mqClient.connected) {
+            return;
+        }
+
+        cacheDrain();
+    }, 100);
+}
+
 
 function addrToBuffer(addr, buffer, index) {
     buffer[index] = parseInt(addr.substring(0, 2), 16);
@@ -110,6 +176,11 @@ function cacheAdd(addr, rssi, name) {
 
     // rssi
     if (rssi < 0) { // ignore invalid RSSI (rssi >= 0)
+		if(addr === '00ea:24:2efea4')
+		{
+			rssi = _kalman.filter(rssi);
+		}
+
         mqEntry.rssi_total += rssi;
         mqEntry.rssi_count += 1;
     }
@@ -136,81 +207,10 @@ function cacheDrain() {
                 buffer[14] = (Math.round(value.rssi_total / value.rssi_count) + 256) % 0xFF;
                 buffer[15] = value.rssi_count;
 
-                console.log(chalk.dim('[MQTT] '), buffer);
+                console.log(chalk.dim('[MQTT ]'), buffer, value.name ? value.name : '');
 
                 _mqClient.publish('scan', buffer);
                 return;
             }
         });
 }
-
-// ----- NOBLE events
-noble
-    .on('stateChange', function(state) {
-        console.log(chalk.dim('[NOBLE]'), 'state is', state);
-        if (state !== 'poweredOn') {
-            throw new Error('bluetooth powered off or not available')
-            return;
-        }
-
-        setup();
-    });
-
-noble.on('discover', function(peripheral) {
-    let addr = normalizeAddr(peripheral.uuid);
-    cacheAdd(addr, peripheral.rssi, peripheral.advertisement.localName);
-});
-
-// ----- setup the global variables
-function setup() {
-    Promise
-        .resolve()
-        .then(() => myAddr)
-        .then(addr => {
-            console.log(chalk.dim('[SETUP]'), '...', chalk.bold(addr));
-            _addr = addr;
-        })
-        .then(() => new Promise(function(resolve, reject) {
-            console.log(chalk.dim('[SETUP]'), 'connecting to MQTT server ...');
-
-            let client = mqtt.connect('mqtt://test1:blahblah12@intelligent-rhino.rmq.cloudamqp.com');
-            client.on('connect', function() {
-                resolve(client);
-            });
-            client.on('error', function(err) {
-                reject(err);
-            });
-
-            client.on('reconnect', function() {
-                console.log(chalk.dim('[MQTT] '), 'mqtt.Client.reconnect()', client);
-            });
-
-            client.on('close', function() {
-                console.log(chalk.dim('[MQTT] '), 'mqtt.Client.close()', client);
-                process.exit();
-            });
-        }))
-        .then(client => {
-            console.log(chalk.dim('[SETUP]'), '...', chalk.bold(client.connected
-                ? 'CONNECTED'
-                : 'DISCONNECTED'));
-            _mqClient = client;
-        })
-        .then(() => {
-            console.log(chalk.dim('[SETUP]'), 'start scanning ...');
-            noble.startScanning([], true);
-        })
-        .catch(err => {
-            console.error(chalk.bold('[ERROR]'));
-            console.error(err);
-            process.exit(1);
-        });
-}
-
-setInterval(function() {
-    if (!_mqCache.length || !_mqClient || !_mqClient.connected) {
-        return;
-    }
-
-    cacheDrain();
-}, 100);
---
+++ kalman.js
@@ -0,0 +1,85 @@
+/**
+ * KalmanFilter
+ * @class
+ * @author Wouter Bulten
+ * @see {@link http://github.com/wouterbulten/kalmanjs}
+ * @version Version: 1.0.0-beta
+ * @copyright Copyright 2015 Wouter Bulten
+ * @license GNU LESSER GENERAL PUBLIC LICENSE v3
+ * @preserve
+ */
+
+module.exports = KalmanFilter;
+
+/**
+ * Create 1-dimensional kalman filter
+ * @param  {Number} options.R Process noise
+ * @param  {Number} options.Q Measurement noise
+ * @param  {Number} options.A State vector
+ * @param  {Number} options.B Control vector
+ * @param  {Number} options.C Measurement vector
+ * @return {KalmanFilter}
+ */
+ function KalmanFilter(R=1, Q=1, A=1, B=0, C=1) {
+    this.R = R; // noise power desirable
+    this.Q = Q; // noise power estimated
+
+    this.A = A;
+    this.C = C;
+    this.B = B;
+    this.cov = NaN;
+    this.x = NaN; // estimated signal without noise
+
+
+    /**
+     * Filter a new value
+     * @param  {Number} z Measurement
+     * @param  {Number} u Control
+     * @return {Number}
+     */
+    this.filter = (z, u = 0) => {
+
+        if (isNaN(this.x)) {
+            this.x = (1 / this.C) * z;
+            this.cov = (1 / this.C) * this.Q * (1 / this.C);
+        } else {
+
+            // Compute prediction
+            const predX = (this.A * this.x) + (this.B * u);
+            const predCov = ((this.A * this.cov) * this.A) + this.R;
+
+            // Kalman gain
+            const K = predCov * this.C * (1 / ((this.C * predCov * this.C) + this.Q));
+
+            // Correction
+            this.x = predX + K * (z - (this.C * predX));
+            this.cov = predCov - (K * this.C * predCov);
+        }
+
+        return this.x;
+    }
+
+    /**
+     * Return the last filtered measurement
+     * @return {Number}
+     */
+    this.lastMeasurement = () => {
+        return this.x;
+    }
+
+    /**
+     * Set measurement noise Q
+     * @param {Number} noise
+     */
+    this.setMeasurementNoise = (noise) => {
+        this.Q = noise;
+    }
+
+    /**
+     * Set the process noise R
+     * @param {Number} noise
+     */
+    this.setProcessNoise = (noise) => {
+        this.R = noise;
+    }
+}

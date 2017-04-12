d9cfca3922178079aca459bb06503d1f4f7497dc
Daniel Wang <ddwang@gmail.com>
2017-04-12 12:20:40 -0700

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,45 @@
 #!/usr/bin/env bash
 
+BOLD='\033[0;1m' 
+NC='\033[0m' # No Color
+
 set -e
 trap times EXIT
 
 { # this ensures the entire script is downloaded #
+
+echo '    _____  _'
+echo '   |__  / (_)  _ __     __ _'
+echo '     / /  | | | '"'"'_ \   / _` |'
+echo '    / /_  | | | | | | | (_| |'
+echo '   /____| |_| |_| |_|  \__, |'
+echo '                       |___/  installer.sh (1.2.4)'
+echo
+
+RELAY_NAME=
+while [[ $RELAY_NAME = "" ]]; do
+	printf "? ${BOLD}Name of this Relay${NC} (e.g. 'Living Rm'): "
+	read -r RELAY_NAME
+	RELAY_NAME="$(echo -e "${RELAY_NAME}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+	if [ "$RELAY_NAME" = "" ]; then
+		printf ">>> ERROR: ${BOLD}relay name cannot be empty${NC}\n\n"
+	fi
+done
+
+SITE_TOKEN=
+while [[ $SITE_TOKEN = "" ]]; do
+	printf "? ${BOLD}Site Token${NC} (copy from Zing dashboard): "
+	read -r SITE_TOKEN
+	SITE_TOKEN="$(echo -e "${SITE_TOKEN}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+	if [ "$SITE_TOKEN" = "" ]; then
+		printf ">>> ERROR: ${BOLD}site token cannot be empty${NC}\n\n"
+	elif [ $(echo -ne "${SITE_TOKEN}" | wc -m) -ne 24 ]; then
+		printf ">>> ERROR: ${BOLD}site token looks weird${NC}\n\n"
+		SITE_TOKEN=
+	fi
+done
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; echo ; exit 1 ; fi
 
@@ -73,11 +109,36 @@ su ${SUDO_USER} <<'EOF_EOF_EOF'
 	echomsg "give Node.js permission to access bluetooth..."
 	sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
 
+	echomsg "installing yarn..."
+	npm install -g yarn
+
 	echomsg "installing zing-relay..."
-	npm install -g zing-relay
+	yarn global add zing-relay
+
+	echomsg "creating starting script for zing-relay..."
+	mkdir -p ~/.zing-relay
+	echo '#!/bin/bash' > ~/.zing-relay/start.sh
+	echo ". ${HOME}/.nvm/nvm.sh" >> ~/.zing-relay/start.sh
 
 EOF_EOF_EOF
-	
+
+	local __ZING_RELAY_SERVICE="/etc/systemd/system/zing-relay.service"
+	echomsg "creating systemd service..."
+	echo "[Unit]" 											> "$__ZING_RELAY_SERVICE"
+	echo "Description=Zing Relay"							>> "$__ZING_RELAY_SERVICE"
+	echo 													>> "$__ZING_RELAY_SERVICE"
+	echo "[Service]" 										>> "$__ZING_RELAY_SERVICE"
+	echo "ExecStart=/home/${SUDO_USER}/start-zing-relay.sh"	>> "$__ZING_RELAY_SERVICE"
+	echo "WorkingDirectory=/home/${SUDO_USER}"				>> "$__ZING_RELAY_SERVICE"
+	echo "IgnoreSIGPIPE=false"								>> "$__ZING_RELAY_SERVICE"
+	echo "KillMode=control-group"							>> "$__ZING_RELAY_SERVICE"
+	echo "User=${SUDO_USER}"								>> "$__ZING_RELAY_SERVICE"
+	echo 													>> "$__ZING_RELAY_SERVICE"
+	echo "[Install]"										>> "$__ZING_RELAY_SERVICE"
+	echo "WantedBy=multi-user.target"						>> "$__ZING_RELAY_SERVICE"
+
+	echo "zing-relay --token \"${SITE_TOKEN}\" --name \"${RELAY_NAME}\"" >> /home/${SUDO_USER}/.zing-relay/start.sh
+
 	echomsg "done, reboot to complete the installation"
 	echo
 }
